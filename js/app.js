bdc34c0c2e0fda40aed1fca06705c33d5e541265
Gabriel Q. Shanahan <41175126+gshanahan@users.noreply.github.com>
2022-09-01 11:57:12 -0500

    Creation of files.

---
+++ js/app.js
@@ -0,0 +1,6 @@
+$('#btnLogin').on('click', function(){
+    let strEmail = document.getElementById('txtEmail').value;
+    let strPassword = $('#txtPassword').val();
+    console.log(strEmail);
+    console.log(strPassword);
+});
